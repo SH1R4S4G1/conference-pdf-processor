ee2f7059f85cb5d290d661c2b4536fafe43dbb90
SH1R4S4G1 <gleipnir_648@wine.ocn.ne.jp>
2023-09-13 22:31:46 +0900

    Remove frontend/.next and frontend/node_modules directory from Git tracking

--- frontend/.next/static/E8L1wx79D8qQnz_qvMdHI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-240c1938f4e48bed.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

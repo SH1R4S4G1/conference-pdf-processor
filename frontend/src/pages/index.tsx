777bee06f1658f141bcf3cf79e721daa1834de41
SH1R4S4G1 <gleipnir_648@wine.ocn.ne.jp>
2023-09-17 17:42:56 +0900

    パターン別にファイルを個別選択する機能の実装、ファイル投入先変更（画面全体）、トグルボタン実装

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@
 // 処理の際には、フルパスを使用する。必要に応じて「path.basename()」を使用してファイル名のみを取得する。
 
 import React, { useState, useEffect } from 'react';
+import { FaRegTrashAlt, FaFileUpload } from 'react-icons/fa';
 import FileRowWithControls from '../components/FileRowWithControls';
 import axios from 'axios';
 import { ipcRenderer } from 'electron';
@@ -12,6 +13,7 @@ type Pattern = {
   id: number;
   addPageNumbers: boolean;
   oddPageFiles: string[];
+  selectedFiles: string[];
 };
 
 type UploadedFile = {
@@ -153,8 +155,9 @@ export default function Home() {
 
         // オリジナルのファイル名を保存
         setUploadedFiles(prev => [...prev, { originalName: file.name, tempPath: outputFilePath }]);
+        setPatterns(prev => prev.map(p => ({ ...p, selectedFiles: [...p.selectedFiles, outputFilePath] })));
 
-        // 処理済みPDFからページ数を取得
+          // 処理済みPDFからページ数を取得
         const pageCount = await window.electron.invoke('get-pdf-page-count', outputFilePath);
         console.log(pageCount);
         if (pageCount % 2 !== 0) {
@@ -171,44 +174,13 @@ export default function Home() {
     }
   };
 
-  // ページ数が奇数のPDFファイルに白紙を差し込む
-  const handleAddBlankPages = async () => {
-    for (const filePath of filesToInsertBlank) {
-      try {
-        const newFilePath = await window.electron.invoke('add-blank-page', filePath);
-        setFileList(prevFiles => [...prevFiles, newFilePath]);
-      } catch (error) {
-        console.error(`Error adding blank page to ${filePath}:`, error);
-      }
-    }
-    console.log('白紙差し込み完了');
-  };
-  
-  
-  const handleCombinePDFs = async () => {
-    try {    
-      const tempFilePath = await window.electron.invoke('combine-pdfs', {
-        files: fileList,
-        addPageNumbers: addPageNumbers
-      });
-
-      await window.electron.invoke('open-file', tempFilePath);
-    } catch (error) {
-      console.error("Error combining PDFs:", error);
-    }
-  };
-
-  const handleInsertBlankCheckboxChange = (filePath: string, isChecked: boolean) => {
-    if (isChecked) {
-      setFilesToInsertBlank(prev => [...prev, filePath]);
-    } else {
-      setFilesToInsertBlank(prev => prev.filter(f => f !== filePath));
-    }
-  };
-
   const handleCombinePDFsForAllPatterns = async () => {
     for (const pattern of patterns) {
-      const combinedFiles = await combineFilesForPattern(pattern);
+    // ここでselectedFilesの内容を参照して処理対象のファイルを絞り込む
+    const filesToProcess = uploadedFiles.filter(file => pattern.selectedFiles.includes(file.tempPath));
+    // const combinedFiles = await handleCombinePDFs(pattern, filesToProcess);
+    const combinedFiles = await combineFilesForPattern(pattern, filesToProcess);
+    
       const tempFilePath = await window.electron.invoke('combine-pdfs', {
         files: combinedFiles,
         addPageNumbers: pattern.addPageNumbers
@@ -217,15 +189,16 @@ export default function Home() {
     }
   };
   
-  const combineFilesForPattern = async (pattern: Pattern) => {
+  const combineFilesForPattern = async (pattern: Pattern, filesToProcess: Array<UploadedFile>) => {
     let combinedFiles: string[] = [];
   
-    for (const file of fileList) {
-      if (pattern.oddPageFiles.includes(file)) {
-        const newFilePath = await window.electron.invoke('add-blank-page', file);
+    for (const file of filesToProcess) {
+      const filePathToProcess = file.tempPath;
+      if (pattern.oddPageFiles.includes(filePathToProcess)) {
+        const newFilePath = await window.electron.invoke('add-blank-page', filePathToProcess);
         combinedFiles.push(newFilePath);  // 白紙を追加した新しいファイルを結合リストに追加
       } else {
-        combinedFiles.push(file);  // 通常のファイルを結合リストに追加
+        combinedFiles.push(filePathToProcess);  // 通常のファイルを結合リストに追加
       }
     }
     return combinedFiles;
@@ -236,6 +209,7 @@ export default function Home() {
       id: Date.now(),
       addPageNumbers: false,
       oddPageFiles: [],
+      selectedFiles: uploadedFiles.map(file => file.tempPath)  // すべてのファイルをデフォルトで選択する
     };
     setPatterns((prevPatterns) => [...prevPatterns, newPattern]);
   };
@@ -274,76 +248,86 @@ export default function Home() {
   };
   
   return (
-    <div className="p-10">
+    <div 
+    className="p-10 h-screen w-screen"
+    onDrop={onDrop}
+    onDragOver={(e) => e.preventDefault()}
+    >  
       <div 
         className="border-dashed border-2 h-60 flex justify-center items-center transition-colors hover:bg-gray-100 cursor-pointer" 
-        onDrop={onDrop} 
-        onDragOver={(e) => e.preventDefault()}
       >
-        PDFをここにドロップ
+        <FaFileUpload size={30} color="gray" className="px-2 py-1 hover:bg-black-100 rounded"/>
+        PDFをドロップ
       </div>
-  
-      <div id="tableWrapper" className="mt-10 relative overflow-x-auto">
-        <table className="w-full border rounded-lg ">
-          <thead>
-            <tr className="text-left">
-              <th className="px-4 py-2 w-1/3 border">ファイル名</th>
-              {patterns.map((pattern, index) => (
-                <th key={pattern.id} className={`px-4 py-2 border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}`}>
-                  パターン {pattern.id}
-                  {patterns.length > 1 && (
-                    <button onClick={() => removePattern(pattern.id)}>削除</button>
-                  )}
-                  <br />
-                  <label className="flex items-center space-x-2 cursor-pointer">
-                    <input
-                      type="checkbox"
-                      className="cursor-pointer"
-                      checked={pattern.addPageNumbers}
-                      onChange={e => {
-                        const updatedPatterns = patterns.map(p => {
-                          if (p.id === pattern.id) {
-                            return { ...p, addPageNumbers: e.target.checked };
-                          }
-                          return p;
-                        });
-                        setPatterns(updatedPatterns);
-                      }}
-                    />
-                    <span>ページ数を付与</span>
-                  </label>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {uploadedFiles.map((file, index) => (
-              <tr key={file.tempPath}>
-                <td className="px-4 py-2 border flex items-center">
-                  {file.originalName}
-                  <FileRowWithControls
-                    fileName={file.originalName}
-                    isFirst={index === 0}
-                    isLast={index === uploadedFiles.length - 1}
-                    onMoveUp={() => handleMoveFileUp(index)}
-                    onMoveDown={() => handleMoveFileDown(index)}
-                    onRemove={() => handleRemoveFile(index)}
-                  />
-                </td>
+
+      {uploadedFiles.length > 0 && (
+        <div id="tableWrapper" className="mt-10 relative overflow-x-auto">
+          <table className="w-full border rounded-lg ">
+            <thead>
+              <tr className="text-left">
+                <th className="px-4 py-2 w-1/3 border">ファイル名</th>
                 {patterns.map((pattern, index) => (
-                  <td key={pattern.id} className={`px-4 py-2 border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}`}>
-                    {oddPageFiles.includes(file.tempPath) && (
+                  <th key={pattern.id} className={`px-4 py-2 border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}`}>
+                    パターン {pattern.id}
+                    {patterns.length > 1 && (
+                      <FaRegTrashAlt size={30} color="red" className="px-2 py-1 hover:bg-red-100 rounded" onClick={() => removePattern(pattern.id)} />
+                    )}
+                    <br />
+                    <label className="flex items-center space-x-2 cursor-pointer">
                       <input
                         type="checkbox"
-                        className="cursor-pointer"
-                        checked={pattern.oddPageFiles.includes(file.tempPath)}
+                        className="cursor-pointer peer sr-only"
+                        checked={pattern.addPageNumbers}
+                        onChange={e => {
+                          const updatedPatterns = patterns.map(p => {
+                            if (p.id === pattern.id) {
+                              return { ...p, addPageNumbers: e.target.checked };
+                            }
+                            return p;
+                          });
+                          setPatterns(updatedPatterns);
+                        }}
+                      />
+                      <span
+                          className="block w-[2em] cursor-pointer bg-gray-500 rounded-full 
+                          p-[1px] after:block after:h-[1em] after:w-[1em] after:rounded-full 
+                          after:bg-white after:transition peer-checked:bg-blue-500 
+                          peer-checked:after:translate-x-[calc(100%-2px)]"
+                        >
+                      </span>
+                      <span>ページ数を付与</span>
+                    </label>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {uploadedFiles.map((file, index) => (
+                <tr key={file.tempPath}>
+                  <td className="px-4 py-2 border flex items-center">
+                    <FileRowWithControls
+                      fileName={file.originalName}
+                      isFirst={index === 0}
+                      isLast={index === uploadedFiles.length - 1}
+                      onMoveUp={() => handleMoveFileUp(index)}
+                      onMoveDown={() => handleMoveFileDown(index)}
+                      onRemove={() => handleRemoveFile(index)}
+                    />
+                  </td>
+                  {patterns.map((pattern, index) => (
+                  <td key={pattern.id} className={`px-4 py-2 border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}`}>
+                    <label className="flex items-center">
+                      <input
+                        type="checkbox"
+                        className="cursor-pointer mr-2"
+                        checked={pattern.selectedFiles?.includes(file.tempPath)}
                         onChange={e => {
                           const updatedPatterns = patterns.map(p => {
                             if (p.id === pattern.id) {
                               if (e.target.checked) {
-                                return { ...p, oddPageFiles: [...p.oddPageFiles, file.tempPath] };
+                                return { ...p, selectedFiles: p.selectedFiles ? [...p.selectedFiles, file.tempPath] : [file.tempPath] };
                               } else {
-                                return { ...p, oddPageFiles: p.oddPageFiles.filter(f => f !== file.tempPath) };
+                                return { ...p, selectedFiles: p.selectedFiles?.filter(f => f !== file.tempPath) };
                               }
                             }
                             return p;
@@ -351,24 +335,58 @@ export default function Home() {
                           setPatterns(updatedPatterns);
                         }}
                       />
+                      <span>統合PDFに追加</span>
+                    </label>
+                    <div className="border-t mt-2 mb-2"></div>
+                    {oddPageFiles.includes(file.tempPath) && (
+                      <label className="flex items-center mt-2">
+                        <input
+                          type="checkbox"
+                          className="cursor-pointer mr-2 peer sr-only"
+                          checked={pattern.oddPageFiles.includes(file.tempPath)}
+                          onChange={e => {
+                            const updatedPatterns = patterns.map(p => {
+                              if (p.id === pattern.id) {
+                                if (e.target.checked) {
+                                  return { ...p, oddPageFiles: [...p.oddPageFiles, file.tempPath] };
+                                } else {
+                                  return { ...p, oddPageFiles: p.oddPageFiles.filter(f => f !== file.tempPath) };
+                                }
+                              }
+                              return p;
+                            });
+                            setPatterns(updatedPatterns);
+                          }}
+                        />
+                        <span
+                          className="block w-[2em] cursor-pointer bg-gray-500 rounded-full 
+                          p-[1px] after:block after:h-[1em] after:w-[1em] after:rounded-full 
+                          after:bg-white after:transition peer-checked:bg-blue-500 
+                          peer-checked:after:translate-x-[calc(100%-2px)]"
+                        >
+                        </span>
+                        <span className="ml-2">白紙差込み</span>
+                      </label>
                     )}
                   </td>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-  
-        {isScrollable && (
-          <div
-          className={`absolute inset-x-0 top-0 h-full bg-white bg-opacity-60 flex justify-center items-center transition-opacity duration-500 ${scrollIndicatorOpacity === 0 ? 'hidden' : 'block'}`}
-          style={{ opacity: scrollIndicatorOpacity }}
-          >
-          <p className="text-lg font-bold">横スクロール可能</p>
-          </div>
-        )}
 
-      </div>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+    
+          {isScrollable && (
+            <div
+            className={`absolute inset-x-0 top-0 h-full bg-white bg-opacity-60 flex justify-center items-center transition-opacity duration-500 ${scrollIndicatorOpacity === 0 ? 'hidden' : 'block'}`}
+            style={{ opacity: scrollIndicatorOpacity }}
+            >
+            <p className="text-lg font-bold">横スクロール可能</p>
+            </div>
+          )}
+
+        </div>
+      )}
 
         {uploadedFiles.length > 0 && (
           <button className="mt-5 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600" onClick={addPattern}>＋</button>
